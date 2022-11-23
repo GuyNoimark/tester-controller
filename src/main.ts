667ba904a7fd0bbdee77d59f61420cd2cbc51e90
Guy Noimark <guy.noimark27@gmail.com>
2022-11-23 11:46:06 +0200

    Deleted unnecessary code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import { app, BrowserWindow, ipcMain } from "electron";
 import * as path from "path";
-import { start } from "repl";
-import { ReadlineParser } from "serialport";
 import { ConnectionStatus } from "./Models/ConnectionState";
 const isDev = require("electron-is-dev");
-// const SerialPort = require("serialport");
-// const serialPort = SerialPort.SerialPort;
 import { SerialPort } from "serialport";
 
 function createWindow() {
@@ -89,82 +85,6 @@ function createWindow() {
       mainWindow.webContents.send("error", res);
     }
   });
-
-  // Detect Arduino and LARIT corresponding ports
-  //   ipcMain.handle("getSerialPorts", async () => {
-  //     // serialPort.list().then((ports: SerialPort[]) => console.log(ports));
-
-  //     const arduinoPath = await serialPort
-  //       .list()
-  //       .then(
-  //         (ports: SerialPort[]) =>
-  //           ports.find((port) => port.pnpId === ARDUINO_PNP_ID)?.path
-  //       );
-  //     const LARITPath = await serialPort
-  //       .list()
-  //       .then(
-  //         (ports: SerialPort[]) =>
-  //           ports.find((port) => port.pnpId === LARIT_PNP_ID)?.path
-  //       );
-
-  //     if (arduinoPath !== undefined && LARITPath !== undefined) {
-  //       const arduino = new serialPort({
-  //         path: arduinoPath,
-  //         baudRate: 115200,
-  //       });
-
-  //       const LARIT = new serialPort({
-  //         path:
-  //           LARITPath !== undefined
-  //             ? LARITPath
-  //             : console.log("LARIT is path is undefined"),
-  //         baudRate: 9600,
-  //       });
-
-  //       console.log("Connected to arduino at port", arduinoPath);
-  //       console.log("Connected to LARIT at port", LARITPath);
-
-  //       arduino.on("readable", function () {
-  //         console.log("Arduino Answer:", arduino.read().toString("utf8"));
-  //       });
-
-  //       ipcMain.on("arduinoRead", (event, message: String) => {
-  //         // arduino.on('readable', function () {
-  //         //     console.log('Data:', arduino.read());
-  //         //   })
-  //       });
-
-  // setInterval(() => {
-  //   LARIT.write("?", function (err: any) {
-  //     if (err) {
-  //       return console.log("LARIT - Error on write: ", err.message);
-  //     }
-  //     // console.log('Sent request to LARIT');
-  //   });
-  // }, 20);
-
-  //       // ipcMain.on('LARITRead', (event, message: String) => {
-  //       //         LARIT.write('?');
-  //       //         LARIT.on('readable', function () {
-  //       //             const data = LARIT.read();
-  //       //           })
-
-  //       // });
-
-  //       // const LARIT = new SerialPort({
-  //       //     path: serialPort.list().then((port: SerialPortType) => port.pnpId === LARIT_PNP_ID ? port : undefined),
-  //       //     baudRate: 115200
-  //       // });
-  //       return ConnectionStatus.BOTH_DEVICES_ARE_CONNECTED;
-  //     } else {
-  //       if (arduinoPath === undefined && LARITPath === undefined)
-  //         return ConnectionStatus.ARDUINO_AND_LARIT_ARE_NOT_CONNECTED;
-  //       if (arduinoPath === undefined)
-  //         return ConnectionStatus.ARDUINO_IS_NOT_CONNECTED;
-  //       if (LARITPath === undefined)
-  //         return ConnectionStatus.LARIT_IS_NOT_CONNECTED;
-  //     }
-  //   });
 }
 
 interface SerialPortData {
@@ -216,6 +136,19 @@ const getSerialPortDevice = async (device: Devices): Promise<SerialPort> => {
   });
 };
 
+// const checkConnection = (mainWindow: BrowserWindow): boolean => {
+//   checkConnectionStatus()
+//     .then((response) => {
+//       return response === ConnectionStatus.BOTH_DEVICES_ARE_CONNECTED
+//         ? true
+//         : false;
+//     })
+//     .catch((error) => {
+//       mainWindow.webContents.send("error", error);
+//       return false;
+//     });
+// };
+
 const startArduinoTest = (arduino: SerialPort, testSetings: string) => {
   // arduino.write('Send Data from GUI - ' + message);
   const settings: String[] = testSetings.split(",");
@@ -226,11 +159,6 @@ const startArduinoTest = (arduino: SerialPort, testSetings: string) => {
   arduino.write(`<${(537.7 / 3) * 5}>`);
 
   console.log("Send Data from GUI - " + settings);
-
-  // const interval = setInterval(function () {
-  //   clearInterval(interval);
-  //   console.log("Waiting");
-  // }, 2000);
 };
 
 const sendSensorValueToArduino = (sensorValue: number, arduino: SerialPort) => {
@@ -247,36 +175,6 @@ const sendSensorValueToArduino = (sensorValue: number, arduino: SerialPort) => {
   });
 };
 
-// const getSensorData = (LARIT: SerialPort): number => {
-//   LARIT.on("data", function (data: Buffer) {
-//     const formattedData: string = data.toString("utf8").slice(0, -3);
-//     return parseFloat(formattedData);
-// console.log('Data:', sensorValue);
-
-// sensorValue !== 0 ? realForceCounter += 1 : realForceCounter == 0 ;
-
-// if (sensorValue !== 0) {
-//   realForceCounter += 1;
-
-//   if (realForceCounter < 2) sensorValue = 0.0;
-// } else {
-//   realForceCounter = 0;
-// }
-
-// arduino.write(sensorValue.toString(), function (err: any) {
-//   if (err) {
-//     return console.log("Arduino - Error on write: ", err.message);
-//   }
-//   // console.log('Sent:', sensorValue)
-// });
-// arduino.on('data', function (data: Buffer ) {
-//   console.log('Arduino message: ' , data);
-// })
-
-// mainWindow.webContents.send("getSensorValue", sensorValue);
-//   });
-// };
-
 const toBytes = (string: String) => {
   const buffer = Buffer.from(string, "utf8");
   const result = Array(buffer.length);
@@ -285,20 +183,6 @@ const toBytes = (string: String) => {
   }
   return result;
 };
-
-// const getPorts = (): SerialPort[] => serialPort.list().then(() => {
-//     // Promise approach
-//     return serialPort.list().then((ports: SerialPort[]) => {
-//         ports.forEach(function (port: SerialPort) {
-//             console.log(port.path, port.pnpId, port.manufacturer);
-//         });
-//         return ports
-//     });
-// });
-
-// const getPorts = async ():Promise<SerialPort[]> =>   {
-
-// }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
